652109e4540705e195799203b5737f384956b56a
gabrielsc1998 <98gabrielsc@gmail.com>
2022-11-10 15:51:35 -0300

    feat: implements create category use case

---
+++ src/category/application/use-cases/create-category.use-case.ts
@@ -0,0 +1,24 @@
+import UseCase from "../../../@seedwork/application/use-case";
+
+import { Category } from "../../domain/entities/category";
+import CategoryRepository from "../../domain/repository/category.repository";
+
+import { CategoryOutput, CategoryOutputMapper } from "../dto/category-output";
+
+export default class CreateCategoryUseCase implements UseCase<Input, Output> {
+  constructor(private categoryRepo: CategoryRepository.Repository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const entity = new Category(input);
+    await this.categoryRepo.insert(entity);
+    return CategoryOutputMapper.toOutput(entity);
+  }
+}
+
+export type Input = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+};
+
+export type Output = CategoryOutput;

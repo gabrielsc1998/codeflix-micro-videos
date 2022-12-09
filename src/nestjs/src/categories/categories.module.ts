f3a914aa913b6427a50dd8b0439079592a1ae416
gabrielsc1998 <98gabrielsc@gmail.com>
2022-12-09 10:51:35 -0300

    chore: removes categories service

--- src/nestjs/src/categories/categories.module.ts
+++ src/nestjs/src/categories/categories.module.ts
@@ -3,7 +3,6 @@ import { SequelizeModule } from '@nestjs/sequelize';
 
 import { CategorySequelize } from '@fc/micro-videos/category/infra';
 
-import { CategoriesService } from './categories.service';
 import { CATEGORY_PROVIDERS } from './category.providers';
 import { CategoriesController } from './categories.controller';
 
@@ -11,7 +10,6 @@ import { CategoriesController } from './categories.controller';
   imports: [SequelizeModule.forFeature([CategorySequelize.CategoryModel])],
   controllers: [CategoriesController],
   providers: [
-    CategoriesService,
     ...Object.values(CATEGORY_PROVIDERS.REPOSITORIES),
     ...Object.values(CATEGORY_PROVIDERS.USE_CASES),
   ],

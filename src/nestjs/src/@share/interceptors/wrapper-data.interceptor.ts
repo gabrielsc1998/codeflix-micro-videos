008fb64b81c37d32c7326d72cd44f711d3ac0f05
gabrielsc1998 <98gabrielsc@gmail.com>
2022-12-09 14:21:50 -0300

    feat: implements wrapper-data interceptor

---
+++ src/nestjs/src/@share/interceptors/wrapper-data.interceptor.ts
@@ -0,0 +1,19 @@
+import {
+  Injectable,
+  CallHandler,
+  NestInterceptor,
+  ExecutionContext,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class WrapperDataInterceptor implements NestInterceptor {
+  intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((body) => {
+        return !body || 'meta' in body ? body : { data: body };
+      }),
+    );
+  }
+}

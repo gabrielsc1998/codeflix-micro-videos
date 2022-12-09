1e725064a363a69d4776383e68130c7e7aa0d6c4
gabrielsc1998 <98gabrielsc@gmail.com>
2022-12-09 10:16:48 -0300

    chore: adds share module in app module

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { ShareModule } from './@share/@share.module';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { CategoriesModule } from './categories/categories.module';
@@ -9,6 +10,6 @@ import { CategoriesModule } from './categories/categories.module';
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [ConfigModule.forRoot(), CategoriesModule, DatabaseModule],
+  imports: [ConfigModule.forRoot(), CategoriesModule, DatabaseModule, ShareModule],
 })
 export class AppModule {}

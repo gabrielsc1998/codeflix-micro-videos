e769146f336fababf2d4ea89975b65884bb29ce2
gabrielsc1998 <98gabrielsc@gmail.com>
2022-12-09 14:20:26 -0300

    feat: implements validation in create-category dto

--- src/nestjs/src/categories/dto/create-category.dto.ts
+++ src/nestjs/src/categories/dto/create-category.dto.ts
@@ -1,7 +1,23 @@
 import { CreateCategoryUseCase } from '@fc/micro-videos/category/application';
 
+import { IsNotEmpty, IsString, IsBoolean, IsOptional } from 'class-validator';
+
+import {
+  mustBeAString,
+  cannotBeEmpty,
+  mustBeABoolean,
+} from '../../@share/errors/errors.messages';
+
 export class CreateCategoryDto implements CreateCategoryUseCase.Input {
+  @IsString({ message: mustBeAString('Name') })
+  @IsNotEmpty({ message: cannotBeEmpty('Name') })
   name: string;
+
+  @IsString({ message: mustBeAString('Description') })
+  @IsOptional()
   description?: string;
+
+  @IsBoolean({ message: mustBeABoolean('IsActive') })
+  @IsOptional()
   is_active?: boolean;
 }

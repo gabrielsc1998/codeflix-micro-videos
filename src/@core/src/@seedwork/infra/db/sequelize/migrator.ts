8c82efa267b688f6e6c522167b32cc913c9abb89
gabrielsc1998 <98gabrielsc@gmail.com>
2022-12-20 10:40:40 -0300

    chore: installs and config umzug - migrator

---
+++ src/@core/src/@seedwork/infra/db/sequelize/migrator.ts
@@ -0,0 +1,26 @@
+import { join } from "path";
+import { Sequelize } from "sequelize";
+import { SequelizeStorage, Umzug } from "umzug";
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  host: ":memory:",
+  logging: true,
+});
+
+const migrator = new Umzug({
+  migrations: {
+    glob: [
+      "*/infra/db/sequelize/migrations/*.{js,ts}",
+      {
+        cwd: join(__dirname, "../../../.."),
+        ignore: ["*.d.ts"],
+      },
+    ],
+  },
+  context: sequelize,
+  storage: new SequelizeStorage({ sequelize }),
+  logger: console,
+});
+
+migrator.runAsCLI();

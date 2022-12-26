1fe405dba9b788f958e06a92b7d561e092286cf6
gabrielsc1998 <98gabrielsc@gmail.com>
2022-12-26 14:10:03 -0300

    chore: updates start.sh - docker initialization

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
+######### @core configs #########
+
+echo "----------- Starting @core configs -----------"
+
 if [ ! -f "./src/@core/.env.test" ]; then
     cp ./src/@core/.env.test.example ./src/@core/.env.test
 fi
 
+######### nestjs configs #########
+
+echo "----------- Starting nestjs configs -----------"
+
+if [ ! -f "./src/nestjs/.env" ]; then
+    cp ./src/nestjs/.env.example ./src/nestjs/.env
+fi
+
+if [ ! -f "./src/nestjs/.env.test" ]; then
+    cp ./src/nestjs/.env.test.example ./src/nestjs/.env.test
+fi
+
+if [ ! -f "./src/nestjs/.env.e2e" ]; then
+    cp ./src/nestjs/.env.e2e.example ./src/nestjs/.env.e2e
+fi
+
 npm install
+
+echo "----------- Building @core -----------"
+
+npm run build -w @fc/micro-videos
 
 tail -f /dev/null
 
